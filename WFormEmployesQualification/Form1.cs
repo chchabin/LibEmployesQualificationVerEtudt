d9d381b86cd7914717760e0d43c908ba15ee5506
chchabin <christophe.chabin@ac-creteil.fr>
2021-05-28 08:48:04 +0200

    travail à faire détaillé dans form1.cs

--- WFormEmployesQualification/Form1.cs
+++ WFormEmployesQualification/Form1.cs
@@ -48,8 +48,9 @@ namespace WFormEmployesQualification
         private Qualification GetQualification(string libelle)
         {
             Qualification q = new Qualification();
-
-
+            // Lire les qualification dans la liste des qualifications dans l'entreprise
+            // Trouver la qualification correspondant à la variable libelle
+            // Copier cet eobjet dans q
             return q;
         }
         /// <summary>
@@ -59,7 +60,8 @@ namespace WFormEmployesQualification
         /// <param name="libelle"></param>
         private void afficheEmploye(string libelle)
         {
-
+            // Appeler la méthode qui donne la liste des employés pour le libellé passé en paramètre
+            // Ajouter chacun des éléments de la liste dans l'objet listBox (cela fonctionne comme le comboBox)
         }
         #endregion
         #region Gestion des évènements
@@ -70,15 +72,26 @@ namespace WFormEmployesQualification
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            EffacerQualification();
-            Qualification q = new Qualification(Convert.ToInt32(textBox1.Text), textBox2.Text);
-            label9.Text = q.ToString();
-            entp.ajoutUneQualification(q);
-            AfficheQualification();
+            EffacerQualification(); // Vide le combobox de ses données 
+            // Instanciation de l'objet qualification à partir des objets textBox
+            Qualification q = new Qualification(Convert.ToInt32(textBox1.Text), textBox2.Text); 
+            label9.Text = q.ToString(); // Afficher oes données dans le label
+            entp.ajoutUneQualification(q); // Ajouter une qualification dans l'objet entreprise
+            AfficheQualification(); // Remplir le comboBox
         }
         /*
-         * 1 - Ajouter l'évènement clique sur ajouter un employé
+         * 1 - Ajouter l'évènement clique sur le bouton ajouter un employé (faire un double clique sur le bouton)
+         * 1.1 - Instanciation de l'objet employé à partir des objets textBox
+         * 1.2 - Récupération de la qualification dans le comboBox (Quelque chose comme n°combobox.SelectedItem.ToString())
+         * 1.3 - Avec la méthode GetQualification (que vous finirez) obtenoir l'objet qualification
+         * 1.4 - Ajoutez cette qualification à l'objet employé
+         * 1.5 - Ajouter l'objet employé dans l'objet entreprise
          * 2 - Ajouter l'évènement Selected item index changed sur la selection du combobox de groupe entreprise
+         * 2.1 - Récupération de la qualification dans le comboBox
+         * 2.2 - Appeler la méthode afficheEmploye pour la valeur choisie
+         * 3 - Effacer les données de tous les comboBix et pas un seul.
+         * 4 - Effacer les données déjà saisies lorsque l'utilisateur clique sur un bouton.
+         * 5 - Ordoner la saisie lorsque l'utilisateur utilise le bouton tabulation du clavier.
          */
         #endregion
     }
